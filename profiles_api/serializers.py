9faf00fa28a4e8612a26534385aabf4856ba3c78
NetDef <netdef@pop-os.localdomain>
2022-11-25 21:31:45 +0300

    djange restfull api devlopment

---
+++ profiles_api/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from profiles_api import models
+
+
+class HelloSerializer(serializers.Serializer):
+    """ Serializer a name field for testing our APIView"""
+    name = serializers.CharField(max_length=10)
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """ Serializers a user profile object"""
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'password')
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {'input_type': 'password'}
+            }
+        }
+
+    def create(self, validated_data):
+        """Create and return a new user"""
+        user = models.UserProfile.objects.create_user(
+            email=validated_data['email'],
+            name=validated_data['name'],
+            password=validated_data['password'])
+        return user
+
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """Serializers profile feed items"""
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id', 'user_profile', 'status_text', 'created_on')
+        extra_kwargs = {'user_profile': {'read_only': True}}
